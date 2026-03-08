864150e00f866ed45d7c0e597221b331d112a2b6
PedroFrasao <157742416+PedroFrasao@users.noreply.github.com>
2026-03-08 01:38:21 -0300

    Refactor app.py to improve code structure

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ import cv2
 import numpy as np
 from mtcnn import MTCNN
 
-# Caminho para o dataset
+
 DATASET_PATH = 'C:\\Users\\pedro\\OneDrive\\Área de Trabalho\\face_Security\\dataset - Copia'
 
-# Define a largura e altura da imagem
+
 IMG_WIDTH, IMG_HEIGHT = 128, 128  # Dimensões originais
 
 
@@ -21,22 +21,19 @@ EPOCHS = 200
 
 
 
-# Inicializa o detector MTCNN
+
 detector = MTCNN()
 
 def detect_and_crop_face(img):
-    # Converte a imagem para RGB
     rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # Detecta rostos
     faces = detector.detect_faces(rgb_img)
-    # Verifica se algum rosto foi detectado
     if len(faces) == 0:
-        return img  # Se não detectar, retorna a imagem original
-    # Recorta a região da face (primeiro rosto detectado)
+        return img  
+    
     for face in faces:
         x, y, w, h = face['box']
         cropped_face = img[y:y+h, x:x+w]
-        return cv2.resize(cropped_face, (128, 128))  # Redimensiona a face cortada
+        return cv2.resize(cropped_face, (128, 128))  
     return img  
 
 
@@ -47,17 +44,17 @@ def detect_and_crop_face(img):
 
 
 
-# Função de pré-processamento para ImageDataGenerator
+
 def preprocess_function(img):
-    # Converte a imagem para BGR para manter a consistência com cv2
+  
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    # Detecta e corta o rosto
+    
     img = detect_and_crop_face(img)
-    # Converte de volta para RGB
+    
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
 
-# Gerador de dados com a função de pré-processamento
+
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     rotation_range=20,
@@ -67,16 +64,15 @@ train_datagen = ImageDataGenerator(
     zoom_range=0.2,
     horizontal_flip=True,
     validation_split=0.2,
-    preprocessing_function=preprocess_function  # Adiciona a função de pré-processamento
+    preprocessing_function=preprocess_function 
 )
 
 valid_datagen = ImageDataGenerator(
     rescale=1./255,
     validation_split=0.2,
-    preprocessing_function=preprocess_function  # Adiciona a função de pré-processamento
-)
+    preprocessing_function=preprocess_function  
 
-# Gerar dados de treino e validação
+
 train_generator = train_datagen.flow_from_directory(
     DATASET_PATH,
     target_size=(IMG_WIDTH, IMG_HEIGHT),
@@ -95,7 +91,7 @@ validation_generator = valid_datagen.flow_from_directory(
 
 
 
-# Camada de MaxPooling personalizada
+
 @tf.keras.utils.register_keras_serializable()  
 class CustomMaxPooling(tf.keras.layers.Layer):
     def __init__(self, pool_size=(1, 1), strides=(4, 4), padding='valid', **kwargs):
@@ -111,7 +107,7 @@ class CustomMaxPooling(tf.keras.layers.Layer):
     
 
     def call(self, inputs):
-        # Aplica a operação de max pooling
+       
         x = tf.nn.max_pool2d(inputs, ksize=self.pool_size, strides=self.strides, padding=self.padding.upper())
         return self.dropout(x)
         #return x
@@ -126,7 +122,6 @@ class CustomMaxPooling(tf.keras.layers.Layer):
         })
         return config
 
-# Definindo a arquitetura da CNN
 model2 = Sequential([
     Conv2D(32, (3, 3), activation="relu", padding="same", input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
     CustomMaxPooling(pool_size=(1, 1), strides=(4, 4), padding='valid'),
@@ -151,13 +146,10 @@ model2 = Sequential([
 
 model2.compile(optimizer='nadam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# Resumo da arquitetura
 model2.summary()
 
-# Early stopping
 early_stopping = EarlyStopping(monitor='val_loss', patience=50, restore_best_weights=True)
 
-# Treinando o modelo
 history = model2.fit(
     train_generator,
     epochs=EPOCHS,
@@ -165,16 +157,13 @@ history = model2.fit(
     callbacks=[early_stopping]
 )
 
-# Salvando o modelo treinado
 model2.save('model2_keras2.keras')
 
 
 import matplotlib.pyplot as plt
 
-# Visualizar a acurácia e a perda ao longo do treinamento
 plt.figure(figsize=(12, 4))
 
-# Acurácia
 plt.subplot(1, 2, 1)
 plt.plot(history.history['accuracy'], label='Treinamento')
 plt.plot(history.history['val_accuracy'], label='Validação')
@@ -183,7 +172,6 @@ plt.xlabel('Épocas')
 plt.ylabel('Acurácia')
 plt.legend()
 
-# Perda
 plt.subplot(1, 2, 2)
 plt.plot(history.history['loss'], label='Treinamento')
 plt.plot(history.history['val_loss'], label='Validação')
@@ -194,3 +182,4 @@ plt.legend()
 
 
 plt.show()
+
