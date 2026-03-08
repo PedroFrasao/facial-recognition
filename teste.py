51d22c3e6e920e522eebb3656a3fd066d4df6a12
PedroFrasao <157742416+PedroFrasao@users.noreply.github.com>
2026-03-08 01:40:18 -0300

    Refactor face detection and model prediction code

--- teste.py
+++ teste.py
@@ -7,25 +7,20 @@ from keras.saving import register_keras_serializable
 from mtcnn import MTCNN
 from tensorflow.keras.regularizers import l2
 
-# Inicializa o detector MTCNN
 detector = MTCNN()
 
 def detect_and_crop_face(img):
-    # Converte a imagem para RGB
     rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # Detecta rostos
     faces = detector.detect_faces(rgb_img)
-    # Verifica se algum rosto foi detectado
-    if len(faces) == 0:
-        return img  # Se não detectar, retorna a imagem original
-    # Recorta a região da face (primeiro rosto detectado)
+=    if len(faces) == 0:
+        return img  
     for face in faces:
         x, y, w, h = face['box']
         cropped_face = img[y:y+h, x:x+w]
-        return cv2.resize(cropped_face, (128, 128))  # Redimensiona a face cortada
+        return cv2.resize(cropped_face, (128, 128)) 
     return img 
 
-# Definir a camada personalizada
+
 @tf.keras.utils.register_keras_serializable()  
 class CustomMaxPooling(tf.keras.layers.Layer):
     def __init__(self, pool_size=(2, 2), strides=(2, 2), padding='valid', **kwargs):
@@ -40,7 +35,7 @@ class CustomMaxPooling(tf.keras.layers.Layer):
         super(CustomMaxPooling, self).build(input_shape)
 
     def call(self, inputs):
-        # Aplica a operação de max pooling
+        
         x = tf.nn.max_pool2d(inputs, ksize=self.pool_size, strides=self.strides, padding=self.padding.upper())
         return self.dropout(x)
 
@@ -56,36 +51,32 @@ class CustomMaxPooling(tf.keras.layers.Layer):
 
 model = load_model('model2_keras2.keras', custom_objects={'CustomMaxPooling': CustomMaxPooling})
 
-# Função para pré-processar a imagem de teste
 def preprocess_image(image_path):
     img = cv2.imread(image_path)
     if img is None:
         raise ValueError(f'Erro ao carregar a imagem: {image_path}')
     img = detect_and_crop_face(img)
     img = img / 255.0  
-    img = np.expand_dims(img, axis=0)  # Expande para (1, 128, 128, 3)
+    img = np.expand_dims(img, axis=0)  
     return img
 
-# Caminho da imagem de teste
+
 test_image_path = 'C:\\Users\\pedro\\Downloads\\teste-2.jpg'
 
-# Pré-processa a imagem de teste
 preprocessed_image = preprocess_image(test_image_path)
 
-# Verifique a forma da imagem redimensionada
+
 print("Forma da imagem após redimensionamento:", preprocessed_image.shape)
 
-# Fazer a previsão
 prediction = model.predict(preprocessed_image)[0][0]
 
-# Exibir a probabilidade da previsão
 print(f'Previsão bruta: {prediction:.6f}')
 print(f'Probabilidade de não ser você: {prediction * 100:.2f}%')
 
-# Converte a previsão em classe binária
 if prediction <= 0.01:
     print('É você!')
 else:
     print('Não é você!')
+
 
 
